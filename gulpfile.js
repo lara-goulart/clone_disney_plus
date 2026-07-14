8ecf001e155ec3a933f67fad1a300190989ccb71
Lara Goulart <laralvgs@gmail.com>
2026-07-14 07:44:02 -0300

    Criando hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const imagemin = require('gulp-imagemin')
 
 function compCss() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({ style: 'compressed' }))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(gulp.dest('./dist/css'))
 }
 
-exports.default = compCss
+function minificarImagem() {
+    return gulp.src('./src/images/**')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+}
+
+exports.default = gulp.parallel(compCss, minificarImagem)
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(compCss))
 }
